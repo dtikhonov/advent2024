8722b4c17199a4efd63d9b14037e18670ad3b612
Dmitri Tikhonov <dtikhonov@litespeedtech.com>
2024-12-05 18:25:34 -0500

    Optimize Day 5, part 2.  Now we're cooking.

--- day5/part2.cpp
+++ day5/part2.cpp
@@ -1,6 +1,7 @@
 /* Day 5, part 2.  The idea is to convert the rules into regular expressions
  * and find violations that way.  Building on Part 1, we now use the offsets
- * of the matches to swap the numbers.
+ * of the matches to swap the numbers.  A large optimization is to only check
+ * the rules that the middle number might violate.
  * 
  * No input validation is performed -- this is not the kind of code to put
  * in production.
@@ -13,7 +14,12 @@
 int
 main (void)
 {
-    std::vector<std::regex> rules;
+    struct rule {
+        rule(char *re, int a, int b) : re(re), a(a), b(b) {}
+        std::regex re;
+        int a, b;
+    };
+    std::vector<rule> rules;
     std::string str;
     unsigned midNumberSum = 0;
     constexpr char *re_fmt = R"(\b(%s)\b(.*)\b(%s)\b)";
@@ -30,37 +36,46 @@ main (void)
         ++b;
         /* The trick is to reverse the numbers to find the violation */
         (void) snprintf(re_buf, sizeof(re_buf), re_fmt, b, a);
-        rules.emplace_back(re_buf);
+        rules.emplace_back(re_buf, atoi(a), atoi(b));
     }
 
     while (std::getline(std::cin, str))
     {
-        int changeCount = 0, commaCount;
-restart:
-        for (auto i = 0; i < rules.size(); ++i)
-        {
-            const auto &rule = rules[i];
-            std::smatch m;
-            if (std::regex_search(str, m, rule))
-            {
-                str = m.prefix().str() + m[3].str() + m[2].str()
-                    + m[1].str() + m.suffix().str();
-                ++changeCount;
-                /* There has to be a better way: this is akin to a Bubble
-                 * sort.  But it works.
-                 */
-                goto restart;
-            }
-        }
-        if (changeCount == 0)
-            continue;
+        int commaCount, midNumber;
+        const char *s;
+        /* Check all rules: we only care about the bad lines */
+        for (const auto &rule : rules)
+            if (std::regex_search(str, rule.re))
+                goto got_a_bad_one;
+        continue;
+got_a_bad_one:
+        /* Since we are only swapping the pairs of numbers, we only care about
+         * the middle number.  We need to get the string to the point until
+         * the middle number does not violate any rules.  This reduces the
+         * number of rules we must check.
+         */
+        /* Find the middle number */
         commaCount = 0;
         for (auto c : str)
             commaCount += c == ',';
-        const char *s = str.c_str();
+recheck:
+        s = str.c_str();
         for (auto i = 0; i < commaCount / 2; ++i)
             s = strchr(s, ',') + 1;
-        midNumberSum += atoi(s);
+        midNumber = atoi(s);
+        for (const auto &rule : rules)
+        {
+            if (midNumber != rule.a && midNumber != rule.b)
+                continue;
+            std::smatch m;
+            if (std::regex_search(str, m, rule.re))
+            {
+                str = m.prefix().str() + std::to_string(rule.a) + m[2].str()
+                    + std::to_string(rule.b) + m.suffix().str();
+                goto recheck;
+            }
+        }
+        midNumberSum += midNumber;
     }
 
     std::cout << midNumberSum << std::endl;
